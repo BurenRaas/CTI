ebf65583add4029bb7302b412ecdfd4cdb0bf529
Ruben <s1190828@student.windesheim.nl>
2025-06-19 18:33:38 +0000

    default 3 lagen

--- W1.tf
+++ W1.tf
@@ -147,7 +147,7 @@ resource "azurerm_subnet" "SSC-DB-SUBNET" {
   name                 = "SSC-DB"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.SSC-VNET.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.3.0/24"]
 }
 
 
