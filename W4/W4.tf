44a167efeb24b125ca5a0a5fd40d9fcb8964791e
Ruben <s1190828@student.windesheim.nl>
2025-06-20 13:51:57 +0000

    week 4 v1.0

--- W4/W4.tf
+++ W4/W4.tf
@@ -14,57 +14,68 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "s1190828"
-  location = "westeurope"
+  name     = "s1190828_storage_demo"
+  location = "northeurope"
 }
   
 ////
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "appstorageacct01"
+#Hot blob storage
+resource "azurerm_storage_account" "website" {
+  name                     = "websitestorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  access_tier              = "Hot"
+  account_replication_type = "LRS"
+}
+
+#Cold blob storage
+resource "azurerm_storage_account" "pdfstorage" {
+  name                     = "pdfstorageacct01"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  access_tier              = "Cold"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "pdfs" {
+  name                  = "pdf-files"
+  storage_account_name  = azurerm_storage_account.pdfstorage.name
+  container_access_type = "private"
+}
+
+#SQL storage
+resource "azurerm_mssql_server" "sql" {
+  name                         = "sqlcrmserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "Welkom01!!"
+}
+
+resource "azurerm_mssql_database" "crm" {
+  name         = "crmdb"
+  server_id    = azurerm_mssql_server.sql.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 1
+  sku_name     = "Basic"
+}
+
+
+#Tablestorage
+resource "azurerm_storage_account" "smslogs" {
+  name                     = "smslogstorage01"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "files" {
-  name                  = "pdf-docs"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
-}
-
-resource "azurerm_cosmosdb_account" "cosmos" {
-  name                = "appcosmosdb01"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-
-  consistency_policy {
-    consistency_level = "Session"
-  }
-
-  geo_location {
-    location          = azurerm_resource_group.rg.location
-    failover_priority = 0
-  }
-}
-
-resource "azurerm_mssql_server" "sql" {
-  name                         = "sqlcrmserver01"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "P@ssword1234!"
-}
-
-resource "azurerm_mssql_database" "crm" {
-  name                = "crmdb"
-  server_id           = azurerm_mssql_server.sql.id
-  collation           = "SQL_Latin1_General_CP1_CI_AS"
-  license_type        = "LicenseIncluded"
-  max_size_gb         = 5
-  sku_name            = "Basic"
+resource "azurerm_storage_table" "sms_logs" {
+  name                 = "smslogs"
+  storage_account_name = azurerm_storage_account.smslogs.name
 }
